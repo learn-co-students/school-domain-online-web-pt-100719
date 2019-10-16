3c369c6044abce257fed811457168f7c5be4ad17
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-16 17:15:19 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(student, level)
+    roster[level] ||= []
+    roster[level] << student
+  end
+  
+  def sort 
+    nu_hash = {}
+  end
+    nu_hash
+  end
+end
+
+school = School.new("Bayside High School")
+school.roster
+school.add_student("Zach Morris", 9)
+school.roster
+school.add_student("AC Slater", 9)
+school.add_student("Kelly Kapowski", 10)
+school.add_student("Screech", 11)
+school.roster
+school.grade(9)
+school.sort
