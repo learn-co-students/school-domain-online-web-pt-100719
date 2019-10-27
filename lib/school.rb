6f682b61a78c375bf59d9f08702cb75070849859
Tariq Bailey <tariqbailey627@gmail.com>
2019-10-27 18:50:29 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
-# code here!
+class School
+
+  def initialize(name)
+  @name = name
+  end
+  attr_accessor :name
+  attr_reader :roster
+  
+  def add_student=(name, grade)
+    @name = name
+    @grade = grade
+    @grade, @name = @add_student
+  end
+  
+  def roster=(roster)
+    @roster = roster
+    
+  end
+  
+end
