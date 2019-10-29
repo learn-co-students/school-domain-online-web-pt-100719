012161abf3a355b8849ae96b539c46d853d5df71
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-10-29 02:11:43 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    if @roster.has_key?(grade)
+      @roster[grade] << name
+    else 
+      @roster[grade] = [name]
+    end
+  end
+  
+  def grade (grade)
+    self.roster[grade]
+  end
+  
+  def sort
+    self.roster.each do |grades, students|
+    students.sort!
+  end
+end
+  
+  
+  
+  
+end
