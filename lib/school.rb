442c99b91a1d0d838a4e7eefd899d5828b942c06
essential-robot-4364 <brandon.james414@gmail.com>
2019-11-10 19:01:17 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+# awesomesauce
+class School
+  attr_accessor :name, :roster
+  
+def initialize(name)
+  @name = name
+  @roster = {}
+  end
+ def add_student(student, how)
+    roster[how] ||= []
+    roster[how] << student
+  end 
+  
+  def grade(how)
+    roster.detect do |x, y| 
+      if x == how
+        return y 
+      end 
+    end 
+  end
+def sort 
+  confused = {}
+  roster.each do |x, y| 
+    confused[x] = y.sort 
+  end 
+  confused
+end 
+  
+end 
+  
